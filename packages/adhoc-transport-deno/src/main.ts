397ff385c912328df0d46b4bf21b7008080bb078
Person <localboxcrox@gmail.com>
2024-04-12 03:31:20 -0400

    stuff

--- packages/adhoc-transport-deno/src/main.ts
+++ packages/adhoc-transport-deno/src/main.ts
@@ -2,11 +2,9 @@ export {
     fetch,
     WebSocket,
     Request,
-    Response,
     type RequestInfo,
     type RequestInit,
-    type RequestInitCfProperties,
     type ResponseInit,
     Headers,
     type HeadersInit
-} from '@cloudflare/workers-types';
+} from './deno.js';
