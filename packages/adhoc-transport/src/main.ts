ad473e412a2a64b52fff59104017bdcd5d5b8f39
Person <localboxcrox@gmail.com>
2024-04-12 04:23:29 -0400

    stuff

--- packages/adhoc-transport/src/main.ts
+++ packages/adhoc-transport/src/main.ts
@@ -1,12 +1,13 @@
-export {
-    fetch,
-    WebSocket,
-    Request,
-    Response,
-    type RequestInfo,
-    type RequestInit,
-    type RequestInitCfProperties,
-    type ResponseInit,
-    Headers,
-    type HeadersInit
-} from '@cloudflare/workers-types';
+// export {
+//     fetch,
+//     WebSocket,
+//     Request,
+//     Response,
+//     type RequestInfo,
+//     type RequestInit,
+//     type RequestInitCfProperties,
+//     type ResponseInit,
+//     Headers,
+//     type HeadersInit
+// } from '@cloudflare/workers-types';
+
