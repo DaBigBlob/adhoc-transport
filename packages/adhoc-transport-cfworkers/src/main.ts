03f56a9aa6597bfc160b4221c724ab6a75520a09
Person <localboxcrox@gmail.com>
2024-04-11 23:38:02 -0400

    stuff

---
+++ packages/adhoc-transport-cfworkers/src/main.ts
@@ -0,0 +1,10 @@
+const _fetch = fetch;
+const _Request = Request;
+const _Headers = Headers;
+const _WebSocket = WebSocket;
+export {
+    _fetch as fetch,
+    _Request as Request,
+    _Headers as Headers,
+    _WebSocket as WebSocket
+};
